39ff48e994089ec3e60cecada87997277e9735bf
abhi-develope <prajapatiabhishek320@gmail.com>
2025-07-28 15:45:29 +0530

    print up to 1 using recursion

---
+++ gfg/basicRecursion/PrintReverseUpToN.java
@@ -0,0 +1,15 @@
+public class PrintReverseUpToN {
+    class Solution {
+
+        void printNos(int N) {
+            // code here
+            if(N < 1){
+                return;
+            }
+            
+            System.out.print(N + " ");
+            printNos(N-1);
+            
+        }
+    }
+}
